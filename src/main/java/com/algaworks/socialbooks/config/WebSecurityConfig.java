fe64a59d4396b817cd3232cb5bf9686bcaae1612
Weliff Lima <weliff.14@gmail.com>
2016-05-02 13:52:37 -0300

    adicionado segurança com HTTP BASIC.

---
+++ src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.algaworks.socialbooks.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("weliff").password("123").roles("USER");
+		
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/h2-console/**").permitAll()
+			.anyRequest()
+			.authenticated()
+		.and()
+			.httpBasic()
+		.and()
+			.csrf().disable();
+	}
+}
