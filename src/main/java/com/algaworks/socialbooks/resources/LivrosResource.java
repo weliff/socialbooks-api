1e5f0d8165ff31e77d3748aeb8419fca294d133c
Weliff Lima <weliff.14@gmail.com>
2016-05-14 12:02:17 -0300

    Adicionado CrossOrigin.

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResource.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResource.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ public class LivrosResource {
 	private LivrosService livrosService;
 	
 	@RequestMapping(method=RequestMethod.GET)
+	@CrossOrigin
 	public ResponseEntity<List<Livro>> listar() {
 		return ResponseEntity.status(HttpStatus.OK).body(livrosService.listar());
 	}
